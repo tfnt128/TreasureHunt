cd5a13830ffa95c95fbd8bf70f48904f893f4f01
Thiago Fontoura <thiagoatf@hotmail.com>
2023-06-05 16:22:15 -0300

    Player Movement + URP + Post Processing + new assets/props

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,18 +1,80 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+
+[RequireComponent(typeof(CharacterController))]
 
 public class PlayerMovement : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [Header("Movement")]
+    [SerializeField] private float walkingSpeed;
+    [SerializeField] private float gravity;
+
+    [Header("Look")]
+    [SerializeField] private float lookSpeedX;
+    [SerializeField] private float lookSpeedY;
+    [SerializeField] private float upperLookLimit;
+    [SerializeField] private float lowerLookLimit;
+
+    private Camera playerCamera;
+    private CharacterController characterController;
+
+    private Vector3 forward;
+    private Vector3 right;
+    private Vector3 moveDirection;
+    private Vector2 currentInput;
+
+    private float rotationX = 0;
+
+    void Awake()
     {
-        
+        playerCamera = GetComponentInChildren<Camera>();
+        characterController = GetComponent<CharacterController>();
+
+        LockCursor(CursorLockMode.Locked);
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        MovementInput();
+
+        MouseLook();
+
+        FinalMovements();
     }
+
+    public void LockCursor(CursorLockMode lockMode)
+    {
+        Cursor.lockState = lockMode;
+        Cursor.visible = lockMode == CursorLockMode.Locked ? false : true;
+    }
+
+    #region Controllers
+    private void MovementInput()
+    {
+        forward = transform.TransformDirection(Vector3.forward);
+        right = transform.TransformDirection(Vector3.right);
+
+        currentInput = new Vector2(walkingSpeed * Input.GetAxis("Vertical"), walkingSpeed * Input.GetAxis("Horizontal"));
+
+        float movementDirectionY = moveDirection.y;
+        moveDirection = (forward * currentInput.x) + (right * currentInput.y);
+        moveDirection.y = movementDirectionY;
+    }
+
+    private void MouseLook()
+    {
+        rotationX += -Input.GetAxis("Mouse Y") * lookSpeedY;
+        rotationX = Mathf.Clamp(rotationX, -upperLookLimit, lowerLookLimit);
+        playerCamera.transform.localRotation = Quaternion.Euler(rotationX, 0, 0);
+        transform.rotation *= Quaternion.Euler(0, Input.GetAxis("Mouse X") * lookSpeedX, 0);
+    }
+
+    private void FinalMovements()
+    {
+        if (!characterController.isGrounded)
+        {
+            moveDirection.y -= gravity * Time.deltaTime;
+        }            
+        characterController.Move(moveDirection * Time.deltaTime);
+    }
+    #endregion
 }
